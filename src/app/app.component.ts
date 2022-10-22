981482117c5b0100c040ab2e12246dc3fa77fe8f
Cristian <palcau.cristi@gmail.com>
2022-10-22 23:29:24 +0300

    cleanup app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,12 @@ import exifr from 'exifr';
 })
 export class AppComponent {
   title = 'photoexifcompare';
-  selectedImage: ImageData | undefined;
-
   constructor(private imageService: ImageService) {}
 
-  processImage(imageInput: any) {
-    var exifr = require('exifr');
-    var images: File = imageInput.files;
-    const reader = new FileReader();
-
-    reader.addEventListener('load', (event: any) => {
-      // this.selectedImage = new ImageData(event.target.result, file);
-      // var file =
-      // this.imageService.uploadImage(this.selectedImage.file).subscribe(
-      //   (res) => {},
-      //   (err) => {}
-      // );
-    });
+  getExif(imageInput: any) {
     for (let image of imageInput.files) {
-      // var blob = reader.readAsDataURL(image);
       exifr.parse(image, true);
       exifr.parse(image).then((output: any) => console.log('output', output));
     }
   }
-}
-
-class ImageData {
-  constructor(public src: string, public file: File) {}
 }
